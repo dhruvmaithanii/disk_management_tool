10b74a674e1ac673705fed5085152b5aae71b220
dhruvmaithanii <156745167+dhruvmaithanii@users.noreply.github.com>
2025-06-25 23:13:16 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from gui.main_window import DiskVisualizer
-import platform
-
-if __name__ == "__main__":
-    if platform.system() != 'Windows':
-        print("This app is only supported on Windows.")
-    else:
-        app = DiskVisualizer()
-        app.mainloop()
